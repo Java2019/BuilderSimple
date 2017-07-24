f08160ec8d4cb77e35159469fd30585859df3745
java2019 <java_2019@mail.ru>
2017-07-24 20:33:23 +0300

    init

--- app/src/main/java/com/nemo/buildersimple/Mother.java
+++ app/src/main/java/com/nemo/buildersimple/Mother.java
@@ -4,4 +4,9 @@ package com.nemo.buildersimple;
  * Created by use on 24.07.17.
  */
 public class Mother {
+    private String name;
+
+    public Mother(String name) {
+        this.name = name;
+    }
 }
