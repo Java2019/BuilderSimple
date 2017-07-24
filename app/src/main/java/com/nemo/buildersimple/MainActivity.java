f96929cddc74f41a3a85a508bc695ee9332d7eb9
java2019 <java_2019@mail.ru>
2017-07-24 20:56:09 +0300

    init

--- app/src/main/java/com/nemo/buildersimple/MainActivity.java
+++ app/src/main/java/com/nemo/buildersimple/MainActivity.java
@@ -2,9 +2,12 @@ package com.nemo.buildersimple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener{
 
     private Family family = new Family();
 
@@ -15,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this, family.toString(), Toast.LENGTH_LONG).show();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.nameFarther:
+
+                break;
+            case R.id.nameMorther:
+
+                break;
+            case R.id.nameDaughter:
+
+                break;
+        }
     }
 }
