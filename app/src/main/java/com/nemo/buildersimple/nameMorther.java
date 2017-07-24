6094e55bb371f430c61d686b13fb15b6904ed324
java2019 <java_2019@mail.ru>
2017-07-24 20:58:36 +0300

    init

---
+++ app/src/main/java/com/nemo/buildersimple/nameMorther.java
@@ -0,0 +1,13 @@
+package com.nemo.buildersimple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nameMorther extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_morther);
+    }
+}
