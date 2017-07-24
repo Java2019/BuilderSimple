9c76d069f7971ea8c111a386f232c3bd2b008e6d
java2019 <java_2019@mail.ru>
2017-07-24 20:36:50 +0300

    init

--- app/src/main/java/com/nemo/buildersimple/Daughter.java
+++ app/src/main/java/com/nemo/buildersimple/Daughter.java
@@ -9,4 +9,12 @@ public class Daughter {
     public Daughter(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
