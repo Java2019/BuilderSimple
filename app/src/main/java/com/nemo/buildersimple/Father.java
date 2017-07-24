b354769727125cb12dd5cc75ca3eccdc1c074e49
java2019 <java_2019@mail.ru>
2017-07-24 20:33:00 +0300

    init

--- app/src/main/java/com/nemo/buildersimple/Father.java
+++ app/src/main/java/com/nemo/buildersimple/Father.java
@@ -4,4 +4,9 @@ package com.nemo.buildersimple;
  * Created by use on 24.07.17.
  */
 public class Father {
+    private String name;
+
+    public Father(String name) {
+        this.name = name;
+    }
 }
