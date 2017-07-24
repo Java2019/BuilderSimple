0043019fded40d731a1d1a66bdfb1082906928a6
java2019 <java_2019@mail.ru>
2017-07-24 20:37:07 +0300

    init

--- app/src/main/java/com/nemo/buildersimple/Mother.java
+++ app/src/main/java/com/nemo/buildersimple/Mother.java
@@ -9,4 +9,12 @@ public class Mother {
     public Mother(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
